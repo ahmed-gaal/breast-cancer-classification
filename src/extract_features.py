8bb429d2bb2aa40b96f307b7b8141bf0c7b8580e
ahmed14-cell <ahmedito14@gmail.com>
2021-02-28 04:22:50 +0300

    perform feature engineering and data processing.

---
+++ src/extract_features.py
@@ -0,0 +1,42 @@
+"""This script extracts features from the dataset."""
+import pandas as pd
+from param import Params
+
+# Create directory to store extracted features.
+Params.features.mkdir(parents=True, exist_ok=True)
+
+# Loading in train and test data
+train_df = pd.read_csv(str(Params.data / 'train.csv'))
+test_df = pd.read_csv(str(Params.data / 'test.csv'))
+
+# Create function to extract and preprocess features from the data.
+
+
+def feature_extraction(dframe):
+    deff = dframe.loc[:, dframe.columns != 'diagnosis']
+    return deff
+
+
+train_features = feature_extraction(train_df)
+test_features = feature_extraction(test_df)
+
+#Preprocess target for training model.
+
+
+def preprocess_target(dframe):
+    specs = {
+        'M': 0, 'B': 1
+    }
+    dframe['diagnosis'] = dframe['diagnosis'].map(specs)
+    dfr = dframe['diagnosis']
+    return dfr
+
+
+train_target = preprocess_target(train_df)
+test_target = preprocess_target(test_df)
+
+# Saving features and target extracted to the features directory
+train_features.to_csv(str(Params.features / 'train_features.csv'), index=None)
+test_features.to_csv(str(Params.features / 'test_features.csv'), index=None)
+train_target.to_csv(str(Params.features / 'train_target.csv'), index=None)
+test_target.to_csv(str(Params.features / 'test_target.csv'), index=None)
