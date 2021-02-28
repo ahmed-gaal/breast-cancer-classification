4c8e0951f6d4c8afefed7c8f59a7b643e39f019b
ahmed14-cell <ahmedito14@gmail.com>
2021-02-28 04:23:24 +0300

    train using random estimator.

---
+++ src/train_model.py
@@ -0,0 +1,22 @@
+"""This script performs model training on the preprocessed data."""
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from param import Params
+
+# Create a directory to save models developed.
+Params.models.mkdir(parents=True, exist_ok=True)
+
+# Loading train features into pandas DataFrame.
+X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
+y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
+
+# Instantiating and fitting the algorithm.
+model = RandomForestClassifier(n_estimators=1000)
+model.fit(X_train, y_train.to_numpy().ravel())
+
+# Saving model in serialized format.
+pickle.dump(model, open(
+    str(Params.models / 'model.pickle'), 'wb'
+))
