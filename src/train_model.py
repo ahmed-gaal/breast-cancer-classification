54a1ecf0d52bc20916dbc87ad03b50f39e9bbfd5
ahmed14-cell <ahmedito14@gmail.com>
2021-03-05 08:47:40 +0300

    Experimenting with Adaboost.

--- src/train_model.py
+++ src/train_model.py
@@ -1,8 +1,7 @@
 """This script performs model training on the preprocessed data."""
 import pickle
 import pandas as pd
-import numpy as np
-from xgboost import XGBClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from param import Params
 
 # Create a directory to save models developed.
@@ -13,13 +12,10 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiating and fitting the algorithm.
-model = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-              colsample_bynode=1, colsample_bytree=1, gamma=0,
-              learning_rate=0.2, max_delta_step=0, max_depth=3,
-              min_child_weight=1, missing=None, n_estimators=200, n_jobs=1,
-              nthread=None, objective='binary:logistic', random_state=0,
-              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
-              silent=None, subsample=1, verbosity=1)
+model = AdaBoostClassifier(
+    algorithm='SAMME.R', base_estimator=None, learning_rate=1.0,
+    n_estimators=500, random_state=None
+)
 model.fit(X_train, y_train.to_numpy().ravel())
 
 # Saving model in serialized format.
