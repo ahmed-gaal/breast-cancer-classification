7743bcdf742ca519430d393fad1a066730006cce
ahmed14-cell <ahmedito14@gmail.com>
2021-03-05 08:50:56 +0300

    Boosting Random Forest.

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,7 @@
 """This script performs model training on the preprocessed data."""
 import pickle
 import pandas as pd
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from param import Params
 
 # Create a directory to save models developed.
@@ -12,8 +12,9 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiating and fitting the algorithm.
+base_rf = RandomForestClassifier(n_estimators=500)
 model = AdaBoostClassifier(
-    algorithm='SAMME.R', base_estimator=None, learning_rate=1.0,
+    algorithm='SAMME.R', base_estimator=base_rf, learning_rate=1.0,
     n_estimators=500, random_state=None
 )
 model.fit(X_train, y_train.to_numpy().ravel())
