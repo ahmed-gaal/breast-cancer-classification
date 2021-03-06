ca96a6943278125c9b6a49e544810e00dc3c8450
ahmed14-cell <ahmedito14@gmail.com>
2021-03-06 16:52:47 +0300

    Experimenting with Adaboost.

--- src/train_model.py
+++ src/train_model.py
@@ -12,10 +12,10 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiating and fitting the algorithm.
-base_rf = RandomForestClassifier(n_estimators=500)
+base_rf = RandomForestClassifier(n_estimators=1000)
 model = AdaBoostClassifier(
-    algorithm='SAMME.R', base_estimator=base_rf, learning_rate=1.0,
-    n_estimators=500, random_state=None
+    algorithm='SAMME.R', base_estimator=None, learning_rate=1.0,
+    n_estimators=1000, random_state=None
 )
 model.fit(X_train, y_train.to_numpy().ravel())
 
